67d956300889fb0b596e4368c91e291835f0519f
l3miage-guendour <rayanne74@hotmail.fr>
2023-03-10 11:28:31 +0100

    tp3angular debut

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'tpangular3-guendour-grange',
+    appId: '1:253504197937:web:a29f9b2264e46829d18c10',
+    storageBucket: 'tpangular3-guendour-grange.appspot.com',
+    apiKey: 'AIzaSyDVR2IWwSqgPaFkvcpxBUXGKupSubxmzk8',
+    authDomain: 'tpangular3-guendour-grange.firebaseapp.com',
+    messagingSenderId: '253504197937',
+  },};
